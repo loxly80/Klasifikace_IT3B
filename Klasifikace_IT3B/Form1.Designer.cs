69b0037c519cec8ae7c319e080a86a8d58a4ee76
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-03-01 13:36:43 +0100

    ještě není konec

--- Klasifikace_IT3B/Form1.Designer.cs
+++ Klasifikace_IT3B/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // lvData
+            // 
+            this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvData.GridLines = true;
+            this.lvData.HideSelection = false;
+            this.lvData.Location = new System.Drawing.Point(0, 0);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(800, 450);
+            this.lvData.TabIndex = 0;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Příjmení a jméno";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Dat. nar.";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Angličtina";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Programování";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Matematika";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Grafika";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvData);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvData;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
     }
 }
 
--- Klasifikace_IT3B/SqlRepository.cs
+++ Klasifikace_IT3B/SqlRepository.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+
 
 namespace Klasifikace_IT3B
 {
     public class SqlRepository
     {
+        private string connString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                      Initial Catalog=Klasifikace;
+                                      Integrated Security=True;
+                                      Connect Timeout=30;
+                                      Encrypt=False;
+                                      TrustServerCertificate=False;
+                                      ApplicationIntent=ReadWrite;
+                                      MultiSubnetFailover=False";
+
         private List<Student> TempStudents()
         {
             List<Student> students = new List<Student>();
-            var student = new Student()
+            var student1 = new Student()
             {
                 Id = 1,
                 Firstname = "Pepa",
@@ -17,15 +28,30 @@ namespace Klasifikace_IT3B
                 Birthday = DateTime.Parse("1.1.2000"),
                 Grades = new List<Grade>()
             };
-
+            students.Add(student1);
+            var student2 = new Student()
+            {
+                Id = 1,
+                Firstname = "Franta",
+                Lastname = "Skočdopole",
+                Birthday = DateTime.Parse("28.2.2000"),
+                Grades = new List<Grade>()
+            };
+            students.Add(student2);
             return students;
         }
 
         public List<Student> GetStudents()
         {
-            List<Student> students = new List<Student>();
+            using (SqlConnection sqlConnection = new SqlConnection(connString))
+            {
+                sqlConnection.Open();
+                //List<Student> students = new List<Student>();
+                sqlConnection.Close();
+            }                
 
-            return students;
+            return TempStudents();
+            //return students;
         }
     }
 }
